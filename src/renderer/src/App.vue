e5124dce4b651e3f389e130bb7c4553d9192147d
Eddie6665 <1430095521@qq.com>
2023-05-08 17:01:29 +0800

    New Feature: 1.添加了两个奇奇怪怪的东西（自己看） 2.添加了黑夜白天模式按钮，具体功能未实现

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,39 +1,102 @@
 <template>
   <router-view v-if="!loginPanel"></router-view>
   <div class="main" v-if="loginPanel">
-    <div class="top-bar"><!-- 顶部栏 -->
-      <div class="drag-bar"></div> <!-- 拖拽栏 -->
-      <div class="window-bar"> <!-- 红 绿 灯  -->
-        <div class="circle-green" @click="windowOperation('Min')" style="width: 17px;height: 17px; background-color:#65bc60;border-radius: 50%;"></div>
-        <div class="circle-yellow" @click="windowOperation('Max')" style="width: 17px;height: 17px; background-color:#e7c168;border-radius: 50%;"></div>
-        <div class="circle-red" @click="windowOperation('Close')" style="width: 17px;height: 17px; background-color:#e36f6c;border-radius: 50%;"></div>
+    <div class="top-bar">
+      <!-- 顶部栏 -->
+      <div class="drag-bar"></div>
+      <!-- 拖拽栏 -->
+      <div class="window-bar">
+        <!-- 红 绿 灯  -->
+        <div
+          class="circle-green"
+          @click="windowOperation('Min')"
+          style="width: 17px; height: 17px; background-color: #65bc60; border-radius: 50%"
+        ></div>
+        <div
+          class="circle-yellow"
+          @click="windowOperation('Max')"
+          style="width: 17px; height: 17px; background-color: #e7c168; border-radius: 50%"
+        ></div>
+        <div
+          class="circle-red"
+          @click="windowOperation('Close')"
+          style="width: 17px; height: 17px; background-color: #e36f6c; border-radius: 50%"
+        ></div>
       </div>
     </div>
     <div class="app-box">
-      <SliderBar></SliderBar> <!-- 侧标栏显示 -->
-      <router-view></router-view><!-- 路由显示 -->
+      <SliderBar></SliderBar>
+      <!-- 侧标栏显示 -->
+      <router-view></router-view
+      ><!-- 路由显示 -->
+    </div>
+    <div class="animate">
+      <div class="loader">
+        <svg viewBox="0 0 80 80">
+          <circle id="test" cx="40" cy="40" r="32"></circle>
+        </svg>
+      </div>
+
+      <div class="loader triangle">
+        <svg viewBox="0 0 86 80">
+          <polygon points="43 8 79 72 7 72"></polygon>
+        </svg>
+      </div>
+
+      <div class="loader">
+        <svg viewBox="0 0 80 80">
+          <rect x="8" y="8" width="64" height="64"></rect>
+        </svg>
+      </div>
+    </div>
+    <div class="love">
+      <p class="text">喜欢本打卡器请给个赞吧</p>
+      <div class="heart">
+        <div class="con-like">
+          <input title="like" type="checkbox" class="like" />
+          <div class="checkmark">
+            <svg viewBox="0 0 24 24" class="outline" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z"
+              ></path>
+            </svg>
+            <svg viewBox="0 0 24 24" class="filled" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Z"
+              ></path>
+            </svg>
+            <svg class="celebrate" width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+              <polygon points="10,10 20,20" class="poly"></polygon>
+              <polygon points="10,50 20,50" class="poly"></polygon>
+              <polygon points="20,80 30,70" class="poly"></polygon>
+              <polygon points="90,10 80,20" class="poly"></polygon>
+              <polygon points="90,50 80,50" class="poly"></polygon>
+              <polygon points="80,80 70,70" class="poly"></polygon>
+            </svg>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import SliderBar from "./components/SliderBar.vue";
-import {ref} from "vue";
+import SliderBar from './components/SliderBar.vue'
+import { ref } from 'vue'
 //登录状态栏
 const loginPanel = ref(false)
 
 //主进程通知主窗口进行渲染
-try{
+try {
   window.electronAPI.changeLoginPanel((_event, value) => {
     loginPanel.value = true
   })
-}catch (e) {
+} catch (e) {
   console.error(e)
 }
 const windowOperation = (op) => {
   window.electronAPI.windowOperate(op)
 }
-
 </script>
 
 <style>
@@ -42,13 +105,13 @@ const windowOperation = (op) => {
   padding: 0;
 }
 .main {
-  margin: 5px 5px ;
+  margin: 5px 5px;
   height: 99vh;
   background-color: #eef2f5ee;
   border-radius: 20px 20px;
   box-shadow: 2px 2px 2px 1px rgba(77, 77, 77, 0.1);
   transform: scale(v-bind(plex));
-  backdrop-filter:blur(10px);
+  backdrop-filter: blur(10px);
 }
 
 .app-box {
@@ -57,7 +120,7 @@ const windowOperation = (op) => {
   height: 97vh;
 }
 
-.drag-bar{
+.drag-bar {
   border-radius: 20px;
   width: 86vw;
   height: 100%;
@@ -65,13 +128,12 @@ const windowOperation = (op) => {
   -webkit-app-region: drag;
 }
 
-.top-bar{
+.top-bar {
   border-radius: 20px;
   height: 6vh;
   display: flex;
   align-items: center;
   justify-content: space-between;
-
 }
 
 .window-bar {
@@ -97,4 +159,259 @@ const windowOperation = (op) => {
 }
 /* */
 
+.animate {
+  position: absolute;
+  right: 5px;
+  top: 60px;
+  transform: scale(0.7);
+}
+
+.love {
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: 5px;
+  left: 400px;
+}
+.heart {
+  transform: scale(0.5);
+}
+
+.loader {
+  --path: #2f3545;
+  --dot: #5628ee;
+  --duration: 3s;
+  width: 44px;
+  height: 44px;
+  position: relative;
+}
+
+.loader:before {
+  content: '';
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  position: absolute;
+  display: block;
+  background: var(--dot);
+  top: 37px;
+  left: 19px;
+  transform: translate(-18px, -18px);
+  animation: dotRect var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+}
+
+.loader svg {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+
+.loader svg rect,
+.loader svg polygon,
+.loader svg circle {
+  fill: none;
+  stroke: var(--path);
+  stroke-width: 10px;
+  stroke-linejoin: round;
+  stroke-linecap: round;
+}
+
+.loader svg polygon {
+  stroke-dasharray: 145 76 145 76;
+  stroke-dashoffset: 0;
+  animation: pathTriangle var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+}
+
+.loader svg rect {
+  stroke-dasharray: 192 64 192 64;
+  stroke-dashoffset: 0;
+  animation: pathRect 3s cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+}
+
+.loader svg circle {
+  stroke-dasharray: 150 50 150 50;
+  stroke-dashoffset: 75;
+  animation: pathCircle var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+}
+
+.loader.triangle {
+  width: 48px;
+}
+
+.loader.triangle:before {
+  left: 21px;
+  transform: translate(-10px, -18px);
+  animation: dotTriangle var(--duration) cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+}
+
+@keyframes pathTriangle {
+  33% {
+    stroke-dashoffset: 74;
+  }
+
+  66% {
+    stroke-dashoffset: 147;
+  }
+
+  100% {
+    stroke-dashoffset: 221;
+  }
+}
+
+@keyframes dotTriangle {
+  33% {
+    transform: translate(0, 0);
+  }
+
+  66% {
+    transform: translate(10px, -18px);
+  }
+
+  100% {
+    transform: translate(-10px, -18px);
+  }
+}
+
+@keyframes pathRect {
+  25% {
+    stroke-dashoffset: 64;
+  }
+
+  50% {
+    stroke-dashoffset: 128;
+  }
+
+  75% {
+    stroke-dashoffset: 192;
+  }
+
+  100% {
+    stroke-dashoffset: 256;
+  }
+}
+
+@keyframes dotRect {
+  25% {
+    transform: translate(0, 0);
+  }
+
+  50% {
+    transform: translate(18px, -18px);
+  }
+
+  75% {
+    transform: translate(0, -36px);
+  }
+
+  100% {
+    transform: translate(-18px, -18px);
+  }
+}
+
+@keyframes pathCircle {
+  25% {
+    stroke-dashoffset: 125;
+  }
+
+  50% {
+    stroke-dashoffset: 175;
+  }
+
+  75% {
+    stroke-dashoffset: 225;
+  }
+
+  100% {
+    stroke-dashoffset: 275;
+  }
+}
+
+.loader {
+  display: inline-block;
+  margin: 0 16px;
+}
+
+
+.con-like {
+  --red: #ee427b;
+  position: relative;
+  width: 50px;
+  height: 50px;
+}
+
+.con-like .like {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  z-index: 20;
+  cursor: pointer;
+}
+
+.con-like .checkmark {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.con-like .outline,
+.con-like .filled {
+  fill: var(--red);
+  position: absolute;
+}
+
+.con-like .filled {
+  animation: kfr-filled 0.5s;
+  display: none;
+}
+
+.con-like .celebrate {
+  position: absolute;
+  animation: kfr-celebrate 0.5s;
+  animation-fill-mode: forwards;
+  display: none;
+}
+
+.con-like .poly {
+  stroke: var(--red);
+  fill: var(--red);
+}
+
+.con-like .like:checked ~ .checkmark .filled {
+  display: block
+}
+
+.con-like .like:checked ~ .checkmark .celebrate {
+  display: block
+}
+
+@keyframes kfr-filled {
+  0% {
+    opacity: 0;
+    transform: scale(0);
+  }
+
+  50% {
+    opacity: 1;
+    transform: scale(1.2);
+  }
+}
+
+@keyframes kfr-celebrate {
+  0% {
+    transform: scale(0);
+  }
+
+  50% {
+    opacity: 0.8;
+  }
+
+  100% {
+    transform: scale(1.2);
+    opacity: 0;
+    display: none;
+  }
+}
 </style>
--- src/renderer/src/Page/Setting.vue
+++ src/renderer/src/Page/Setting.vue
@@ -1,27 +1,40 @@
 <template>
   <div class="menu">
-    <div class="title" >
-      设置功能
-    </div>
+    <div class="title">设置功能</div>
     <div class="white-box">
-      <span class="demonstration">进度条1(&lt;24h)渐变色0%</span><el-color-picker v-model="timerStore.progressColor.big.start" />
-      <span class="demonstration">进度条1(&lt;24h)渐变色100%</span><el-color-picker v-model="timerStore.progressColor.big.end" />
-      <br>
-      <span class="demonstration">进度条2(&lt;48h)渐变色0%</span><el-color-picker v-model="timerStore.progressColor.medium.start" />
-      <span class="demonstration">进度条2(&lt;48h)渐变色100%</span><el-color-picker v-model="timerStore.progressColor.medium.end" />
-      <br>
-      <span class="demonstration">进度条3(&lt;72h)渐变色0%</span><el-color-picker v-model="timerStore.progressColor.small.start" />
-      <span class="demonstration">进度条3(&lt;72h)渐变色100%</span><el-color-picker v-model="timerStore.progressColor.small.end" />
-      <br>
-      <el-button plain type="success" @click="ColorSelector">想自己取个色？</el-button><br>
+      <span class="demonstration">进度条1(&lt;24h)渐变色0%</span
+      ><el-color-picker v-model="timerStore.progressColor.big.start" />
+      <span class="demonstration">进度条1(&lt;24h)渐变色100%</span
+      ><el-color-picker v-model="timerStore.progressColor.big.end" />
+      <br />
+      <span class="demonstration">进度条2(&lt;48h)渐变色0%</span
+      ><el-color-picker v-model="timerStore.progressColor.medium.start" />
+      <span class="demonstration">进度条2(&lt;48h)渐变色100%</span
+      ><el-color-picker v-model="timerStore.progressColor.medium.end" />
+      <br />
+      <span class="demonstration">进度条3(&lt;72h)渐变色0%</span
+      ><el-color-picker v-model="timerStore.progressColor.small.start" />
+      <span class="demonstration">进度条3(&lt;72h)渐变色100%</span
+      ><el-color-picker v-model="timerStore.progressColor.small.end" />
+      <br />
+      <el-button plain type="success" @click="ColorSelector">想自己取个色？</el-button><br />
+      <div class="dark-mode">
+        <p class="text">切换黑夜/白天模式</p>
+        <div class="switch">
+          <label class="switch">
+            <input type="checkbox" />
+            <span class="slider"></span>
+          </label>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import '../assets/css/common.css'
-import {TimerStore} from "../stores/Timer";
-import {ElNotification} from "element-plus";
+import { TimerStore } from '../stores/Timer'
+import { ElNotification } from 'element-plus'
 
 const props = defineProps(['text'])
 
@@ -29,32 +42,89 @@ const timerStore = TimerStore()
 const ColorSelector = async () => {
   ElNotification({
     title: '开始取色',
-    message:'选取任意区域开始选色，按ESC取消',
-    type:'info'
+    message: '选取任意区域开始选色，按ESC取消',
+    type: 'info'
   })
-  let color = await window.electronAPI.saveColorToClipboard();
-  if(color){
-    let message = `颜色代码:${color},已经帮你复制到剪切板上了`;
+  let color = await window.electronAPI.saveColorToClipboard()
+  if (color) {
+    let message = `颜色代码:${color},已经帮你复制到剪切板上了`
     ElNotification({
       title: '取色结果',
       message,
-      type:'success'
+      type: 'success'
     })
-  }else {
+  } else {
     ElNotification({
       title: '取色取消',
-      type:'info'
+      type: 'info'
     })
   }
 }
-
 </script>
 
 <style scoped>
 .demonstration {
   margin-right: 16px;
 }
-.white-box *{
-  padding: 10px;
+.white-box {
+  padding: 15px;
+}
+
+.text {
+  margin-right: 10px;
+}
+.dark-mode {
+  display: flex;
+  align-items: center;
+}
+.switch {
+  font-size: 17px;
+  position: relative;
+  display: inline-block;
+  width: 3.5em;
+  height: 1.95em;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  --background: #28096b;
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: var(--background);
+  transition: .5s;
+  border-radius: 30px;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 1.4em;
+  width: 1.4em;
+  border-radius: 50%;
+  left: 10%;
+  bottom: 15%;
+  box-shadow: inset 8px -4px 0px 0px #fff000;
+  background: var(--background);
+  transition: .5s;
+}
+
+input:checked + .slider {
+  background-color: #522ba7;
+}
+
+input:checked + .slider:before {
+  transform: translateX(100%);
+  box-shadow: inset 15px -4px 0px 15px #fff000;
 }
 </style>
