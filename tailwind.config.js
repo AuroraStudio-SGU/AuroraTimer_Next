d663577fde3dcc7474fee70b9cbeb67f2d681eb7
DAYGood_Time <35392263+DAYGoodTime@users.noreply.github.com>
2023-05-11 20:09:23 +0800

    waiting for prefiex change

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: ['./src/renderer/index.html', './src/renderer/src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
+  // prefix:'tw-',
   theme: {
     extend: {},
   },
@@ -11,5 +12,7 @@ module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    // logs:false,
+    // prefix: 'daisy-'
   },
 }
