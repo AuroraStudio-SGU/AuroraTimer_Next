5458ab86f0385288baf880b4cdfb6f80d984e83e
Eddie6665 <1430095521@qq.com>
2023-06-03 01:07:22 +0800

    排行榜重做

--- src/renderer/src/Page/NewUI.vue
+++ src/renderer/src/Page/NewUI.vue
@@ -1,78 +1,85 @@
 <template>
   <div class="menu">
     <div
-      class="white-box overflow-auto"
-      style="display: flex; align-items: center; justify-content: center"
+      class="white-box overflow-auto flex-col"
+      style="display: flex; align-items: center"
     >
-      <article class="leaderboard text-primary">
-        <header>
-          <svg
-            version="1.1"
-            id="Layer_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            viewBox="0 0 511.999 511.999"
-            style="enable-background: new 0 0 511.999 511.999"
-            xml:space="preserve"
-            class="leaderboard__icon"
-          >
-            <g>
-              <g>
+      <div class="flex"><p class="text-4xl mb-8">卷王榜</p></div>
+      <div class="flex justify-around w-full h-full items-baseline">
+        <div class="flex justify-center items-center flex-col">
+          <div class="avatar">
+            <div
+              class="w-44 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+            >
+              <img :src="getUrl('keli.jpg')" />
+            </div>
+          </div>
+          
+            <button class="btn btn-primary flex text-2xl btn-lg mt-6">
+              <p>陈典灿</p>
+              <svg
+                t="1685707465843"
+                class="icon"
+                viewBox="0 0 1024 1024"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                p-id="3414"
+                width="40"
+                height="40"
+              >
                 <path
-                  d="M466.45,49.374c-7.065-8.308-17.368-13.071-28.267-13.071H402.41v-11.19C402.41,11.266,391.143,0,377.297,0H134.705
-			c-13.848,0-25.112,11.266-25.112,25.112v11.19H73.816c-10.899,0-21.203,4.764-28.267,13.071
-			c-6.992,8.221-10.014,19.019-8.289,29.624c9.4,57.8,45.775,108.863,97.4,136.872c4.717,11.341,10.059,22.083,16.008,32.091
-			c19.002,31.975,42.625,54.073,68.627,64.76c2.635,26.644-15.094,51.885-41.794,57.9c-0.057,0.013-0.097,0.033-0.153,0.046
-			c-5.211,1.245-9.09,5.921-9.09,11.513v54.363h-21.986c-19.602,0-35.549,15.947-35.549,35.549v28.058
-			c0,6.545,5.305,11.85,11.85,11.85H390.56c6.545,0,11.85-5.305,11.85-11.85v-28.058c0-19.602-15.947-35.549-35.549-35.549h-21.988
-			V382.18c0-5.603-3.893-10.286-9.118-11.52c-0.049-0.012-0.096-0.028-0.145-0.04c-26.902-6.055-44.664-31.55-41.752-58.394
-			c25.548-10.86,48.757-32.761,67.479-64.264c5.949-10.009,11.29-20.752,16.008-32.095c51.622-28.01,87.995-79.072,97.395-136.87
-			C476.465,68.392,473.443,57.595,466.45,49.374z M60.652,75.192c-0.616-3.787,0.431-7.504,2.949-10.466
-			c2.555-3.004,6.277-4.726,10.214-4.726h35.777v21.802c0,34.186,4.363,67.3,12.632,97.583
-			C89.728,153.706,67.354,116.403,60.652,75.192z M366.861,460.243c6.534,0,11.85,5.316,11.85,11.85v16.208H134.422v-16.208
-			c0-6.534,5.316-11.85,11.85-11.85H366.861z M321.173,394.03v42.513H191.96V394.03H321.173z M223.037,370.331
-			c2.929-3.224,5.607-6.719,8.002-10.46c7.897-12.339,12.042-26.357,12.228-40.674c4.209,0.573,8.457,0.88,12.741,0.88
-			c4.661,0,9.279-0.358,13.852-1.036c0.27,19.239,7.758,37.45,20.349,51.289H223.037z M378.709,81.803
-			c0,58.379-13.406,113.089-37.747,154.049c-23.192,39.03-53.364,60.525-84.956,60.525c-31.597,0-61.771-21.494-84.966-60.523
-			c-24.342-40.961-37.748-95.671-37.748-154.049V25.112c0-0.78,0.634-1.413,1.412-1.413h242.591c0.78,0,1.414,0.634,1.414,1.413
-			V81.803z M451.348,75.192c-6.702,41.208-29.074,78.51-61.569,104.191c8.268-30.283,12.631-63.395,12.631-97.58V60.001h35.773
-			c3.938,0,7.66,1.723,10.214,4.726C450.915,67.688,451.963,71.405,451.348,75.192z"
-                />
-              </g>
-            </g>
-            <g>
-              <g>
+                  d="M287.762 124.318v252.871c0 123.331 100.905 224.238 224.238 224.238 123.331 0 224.238-100.907 224.238-224.238V124.318H287.762z"
+                  fill="#82A0AF"
+                  p-id="3415"
+                ></path>
                 <path
-                  d="M327.941,121.658c-1.395-4.288-5.103-7.414-9.566-8.064l-35.758-5.196l-15.991-32.402
-			c-1.997-4.044-6.116-6.605-10.626-6.605c-4.511,0-8.63,2.561-10.626,6.605l-15.991,32.402l-35.758,5.196
-			c-4.464,0.648-8.172,3.775-9.566,8.065c-1.393,4.291-0.231,8.999,2.999,12.148l25.875,25.221l-6.109,35.613
-			c-0.763,4.446,1.064,8.938,4.714,11.59c3.648,2.651,8.487,3,12.479,0.902L256,190.32l31.982,16.813
-			c1.734,0.911,3.627,1.36,5.512,1.36c2.456,0,4.902-0.763,6.966-2.263c3.65-2.652,5.477-7.144,4.714-11.59l-6.109-35.613
-			l25.875-25.221C328.172,130.658,329.334,125.949,327.941,121.658z M278.064,146.405c-2.793,2.722-4.068,6.644-3.408,10.489
-			l3.102,18.09l-16.245-8.541c-1.725-0.908-3.62-1.36-5.514-1.36c-1.894,0-3.788,0.454-5.514,1.36l-16.245,8.541l3.102-18.09
-			c0.66-3.844-0.615-7.766-3.408-10.489l-13.141-12.81l18.162-2.64c3.859-0.56,7.196-2.985,8.922-6.482l8.123-16.458l8.122,16.458
-			c1.727,3.497,5.062,5.921,8.922,6.482l18.162,2.64L278.064,146.405z"
-                />
-              </g>
-            </g>
-          </svg>
-
-          <h1 class="leaderboard__title">
-            <span class="leaderboard__title--top">极光工作室</span
-            ><span class="leaderboard__title--bottom">卷王榜</span>
-          </h1>
-        </header>
-
-        <main class="leaderboard__profiles">
-          <article class="leaderboard__profile">
-            <img
-              :src="getUrl('profile.jpg')"
-              alt="Mark Zuckerberg"
-              class="leaderboard__picture"
-            />
-            <span class="leaderboard__name">黄梓聪</span>
+                  d="M379.487 124.318v149.434c0 72.882 59.63 132.513 132.513 132.513 72.882 0 132.513-59.631 132.513-132.513V124.318H379.487z"
+                  fill="#E0EAED"
+                  p-id="3416"
+                ></path>
+                <path
+                  d="M644.513 124.318v149.434c0 72.882-59.631 132.514-132.513 132.514-72.884 0-132.514-59.631-132.514-132.514V124.318h265.027m19.201-19.201H360.285v168.635c0 83.656 68.059 151.714 151.715 151.714s151.714-68.059 151.714-151.714V105.117z"
+                  fill="#FFFFFF"
+                  p-id="3417"
+                ></path>
+                <path
+                  d="M512 622.736m-339.055 0a339.055 339.055 0 1 0 678.11 0 339.055 339.055 0 1 0-678.11 0Z"
+                  fill="#9EC2F7"
+                  p-id="3418"
+                ></path>
+                <path
+                  d="M512 622.736m-250.074 0a250.074 250.074 0 1 0 500.148 0 250.074 250.074 0 1 0-500.148 0Z"
+                  fill="#DDECF7"
+                  p-id="3419"
+                ></path>
+                <path
+                  d="M512 372.662c138.112 0 250.074 111.962 250.074 250.074S650.112 872.81 512 872.81c-138.113 0-250.074-111.962-250.074-250.074S373.888 372.662 512 372.662m0-24.001c-151.126 0-274.075 122.95-274.075 274.075S360.874 896.811 512 896.811s274.075-122.95 274.075-274.076S663.126 348.661 512 348.661z"
+                  fill="#FFFFFF"
+                  p-id="3420"
+                ></path>
+                <path
+                  d="M786.015 124.319h-548.03c-17.08 0-31.055-13.975-31.055-31.055s13.975-31.055 31.055-31.055h548.029c17.08 0 31.055 13.975 31.055 31.055 0.001 17.08-13.974 31.055-31.054 31.055z"
+                  fill="#231815"
+                  p-id="3421"
+                ></path>
+                <path
+                  d="M623.478 748.689v42.438h-218.7v-34.684l128.12-130.562c13.599-13.599 23.256-25.839 28.97-36.723 5.71-10.875 8.569-22.576 8.569-35.092 0-17.949-5.241-32.233-15.708-42.836-10.477-10.613-24.685-15.915-42.639-15.915-16.594 0-30.465 4.285-41.617 12.849-11.157 8.569-19.177 20.748-24.071 36.517l-37.947-22.435c7.346-22.576 19.993-39.785 37.947-51.616 17.949-11.832 40.394-17.752 67.32-17.752 20.945 0 39.644 4.285 56.102 12.854 16.454 8.569 29.237 20.542 38.355 35.908 9.108 15.371 13.669 32.711 13.669 52.024 0 18.498-4.631 35.43-13.876 50.796-9.249 15.371-24.343 33.803-45.288 55.287l-86.09 88.944h146.884z"
+                  p-id="3422"
+                ></path>
+              </svg>
+            </button>
+          
+        </div>
+        <div class="flex justify-center items-center flex-col">
+          <div class="avatar">
+            <div
+              class="w-60 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+            >
+              <img :src="getUrl('caos.jpg')" />
+            </div>
+          </div>
+          <button class="btn btn-secondary flex text-3xl btn-lg mt-6">
+            <p>黄梓聪</p>
             <svg
               t="1685707164518"
               class="icon"
@@ -80,8 +87,8 @@
               version="1.1"
               xmlns="http://www.w3.org/2000/svg"
               p-id="2359"
-              width="30"
-              height="30"
+              width="40"
+              height="40"
             >
               <path
                 d="M287.762 124.318v252.871c0 123.331 100.905 224.238 224.238 224.238 123.331 0 224.238-100.907 224.238-224.238V124.318H287.762z"
@@ -123,76 +130,18 @@
                 p-id="2367"
               ></path>
             </svg>
-            <span class="leaderboard__value">100<span>H</span></span>
-          </article>
-
-          <article class="leaderboard__profile">
-            <img
-              :src="getUrl('profile.jpg')"
-              alt="Dustin Moskovitz"
-              class="leaderboard__picture"
-            />
-            <span class="leaderboard__name">陈典灿</span>
-            <svg
-              t="1685707465843"
-              class="icon"
-              viewBox="0 0 1024 1024"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              p-id="3414"
-              width="30"
-              height="30"
+          </button>
+        </div>
+        <div class="flex justify-center items-center flex-col">
+          <div class="avatar">
+            <div
+              class="w-44 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
             >
-              <path
-                d="M287.762 124.318v252.871c0 123.331 100.905 224.238 224.238 224.238 123.331 0 224.238-100.907 224.238-224.238V124.318H287.762z"
-                fill="#82A0AF"
-                p-id="3415"
-              ></path>
-              <path
-                d="M379.487 124.318v149.434c0 72.882 59.63 132.513 132.513 132.513 72.882 0 132.513-59.631 132.513-132.513V124.318H379.487z"
-                fill="#E0EAED"
-                p-id="3416"
-              ></path>
-              <path
-                d="M644.513 124.318v149.434c0 72.882-59.631 132.514-132.513 132.514-72.884 0-132.514-59.631-132.514-132.514V124.318h265.027m19.201-19.201H360.285v168.635c0 83.656 68.059 151.714 151.715 151.714s151.714-68.059 151.714-151.714V105.117z"
-                fill="#FFFFFF"
-                p-id="3417"
-              ></path>
-              <path
-                d="M512 622.736m-339.055 0a339.055 339.055 0 1 0 678.11 0 339.055 339.055 0 1 0-678.11 0Z"
-                fill="#9EC2F7"
-                p-id="3418"
-              ></path>
-              <path
-                d="M512 622.736m-250.074 0a250.074 250.074 0 1 0 500.148 0 250.074 250.074 0 1 0-500.148 0Z"
-                fill="#DDECF7"
-                p-id="3419"
-              ></path>
-              <path
-                d="M512 372.662c138.112 0 250.074 111.962 250.074 250.074S650.112 872.81 512 872.81c-138.113 0-250.074-111.962-250.074-250.074S373.888 372.662 512 372.662m0-24.001c-151.126 0-274.075 122.95-274.075 274.075S360.874 896.811 512 896.811s274.075-122.95 274.075-274.076S663.126 348.661 512 348.661z"
-                fill="#FFFFFF"
-                p-id="3420"
-              ></path>
-              <path
-                d="M786.015 124.319h-548.03c-17.08 0-31.055-13.975-31.055-31.055s13.975-31.055 31.055-31.055h548.029c17.08 0 31.055 13.975 31.055 31.055 0.001 17.08-13.974 31.055-31.054 31.055z"
-                fill="#231815"
-                p-id="3421"
-              ></path>
-              <path
-                d="M623.478 748.689v42.438h-218.7v-34.684l128.12-130.562c13.599-13.599 23.256-25.839 28.97-36.723 5.71-10.875 8.569-22.576 8.569-35.092 0-17.949-5.241-32.233-15.708-42.836-10.477-10.613-24.685-15.915-42.639-15.915-16.594 0-30.465 4.285-41.617 12.849-11.157 8.569-19.177 20.748-24.071 36.517l-37.947-22.435c7.346-22.576 19.993-39.785 37.947-51.616 17.949-11.832 40.394-17.752 67.32-17.752 20.945 0 39.644 4.285 56.102 12.854 16.454 8.569 29.237 20.542 38.355 35.908 9.108 15.371 13.669 32.711 13.669 52.024 0 18.498-4.631 35.43-13.876 50.796-9.249 15.371-24.343 33.803-45.288 55.287l-86.09 88.944h146.884z"
-                p-id="3422"
-              ></path>
-            </svg>
-            <span class="leaderboard__value">17.9<span>H</span></span>
-          </article>
-
-          <article class="leaderboard__profile">
-            <img
-              :src="getUrl('profile.jpg')"
-              alt="Elizabeth Holmes"
-              class="leaderboard__picture"
-            />
-            <span class="leaderboard__name">欧润丰</span>
+              <img :src="getUrl('hutao.jpg')" />
+            </div>
+          </div>
+          <button class="btn btn-accent flex text-2xl btn-lg mt-6">
+            <p>欧润丰</p>
             <svg
               t="1685707507600"
               class="icon"
@@ -200,8 +149,8 @@
               version="1.1"
               xmlns="http://www.w3.org/2000/svg"
               p-id="4213"
-              width="30"
-              height="30"
+              width="40"
+              height="40"
             >
               <path
                 d="M287.762 124.318v252.871c0 123.331 100.905 224.238 224.238 224.238 123.331 0 224.238-100.907 224.238-224.238V124.318H287.762z"
@@ -243,10 +192,9 @@
                 p-id="4221"
               ></path>
             </svg>
-            <span class="leaderboard__value">1<span>H</span></span>
-          </article>
-        </main>
-      </article>
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -279,120 +227,4 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.leaderboard {
-  max-width: 490px;
-  width: 100%;
-  border-radius: 12px;
-}
-.leaderboard header {
-  --start: 15%;
-  height: 130px;
-  background-image: repeating-radial-gradient(
-      circle at var(--start),
-      transparent 0%,
-      transparent 10%,
-      rgba(54, 89, 219, 0.33) 10%,
-      rgba(54, 89, 219, 0.33) 17%
-    ),
-    linear-gradient(to right, hsl(var(--b1) / var(--tw-bg-opacity)), hsl(var(--b3, var(--b2)) / var(--tw-bg-opacity)));
-  color: hsl(var(--p) / var(--tw-bg-opacity));
-  position: relative;
-  border-radius: 12px 12px 0 0;
-  overflow: hidden;
-}
-.leaderboard header .leaderboard__title {
-  position: absolute;
-  z-index: 2;
-  top: 50%;
-  right: calc(var(--start) * 0.75);
-  transform: translateY(-50%);
-  text-transform: uppercase;
-  margin: 0;
-}
-.leaderboard header .leaderboard__title span {
-  display: block;
-}
-.leaderboard header .leaderboard__title--top {
-  font-size: 24px;
-  font-weight: 700;
-  letter-spacing: 6.5px;
-}
-.leaderboard header .leaderboard__title--bottom {
-  font-size: 13px;
-  font-weight: 500;
-  letter-spacing: 3.55px;
-  opacity: 0.65;
-  transform: translateY(-2px);
-}
-.leaderboard header .leaderboard__icon {
-  fill: hsl(var(--p) / var(--tw-bg-opacity));
-  opacity: 0.35;
-  width: 50px;
-  position: absolute;
-  top: 50%;
-  left: var(--start);
-  transform: translate(-50%, -50%);
-}
-.leaderboard__profiles {
-  background-color: hsl(var(--p) / var(--tw-bg-opacity));
-  border-radius: 0 0 12px 12px;
-  padding: 15px 15px 20px;
-  display: grid;
-  row-gap: 8px;
-}
-.leaderboard__profile {
-  display: grid;
-  grid-template-columns: 1fr 3fr 7fr 1fr;
-  align-items: center;
-  padding: 10px 30px 10px 10px;
-  overflow: hidden;
-  border-radius: 10px;
-  box-shadow: 0 5px 7px -1px rgba(51, 51, 51, 0.23);
-  cursor: pointer;
-  transition: transform 0.25s cubic-bezier(0.7, 0.98, 0.86, 0.98),
-    box-shadow 0.25s cubic-bezier(0.7, 0.98, 0.86, 0.98);
-  background-color: hsl(var(--b3, var(--b2)) / var(--tw-bg-opacity));
-}
-.leaderboard__profile:hover {
-  transform: scale(1.2);
-  box-shadow: 0 9px 47px 11px rgba(51, 51, 51, 0.18);
-}
-.leaderboard__picture {
-  max-width: 100%;
-  width: 60px;
-  border-radius: 50%;
-  box-shadow: 0 0 0 10px hsl(var(--b3, var(--b2)) / var(--tw-bg-opacity)), 0 0 0 22px hsl(var(--b1) / var(--tw-bg-opacity));
-}
-.leaderboard__name {
-  color: hsl(var(--p) / var(--tw-text-opacity));
-  font-weight: 600;
-  font-size: 20px;
-  letter-spacing: 0.64px;
-  margin-left: 12px;
-}
-.leaderboard__value {
-  color: hsl(var(--p) / var(--tw-text-opacity));
-  font-weight: 700;
-  font-size: 34px;
-  text-align: right;
-}
-.leaderboard__value > span {
-  opacity: 0.8;
-  font-weight: 600;
-  font-size: 13px;
-  margin-left: 3px;
-}
-
-body {
-  margin: 0;
-  background-color: #eaeaea;
-  display: grid;
-  height: 100vh;
-  place-items: center;
-
-}
-
-.leaderboard {
-  box-shadow: 0 0 40px -10px rgba(0, 0, 0, 0.4);
-}
 </style>
--- src/renderer/src/components/NavBar.vue
+++ src/renderer/src/components/NavBar.vue
@@ -1,42 +1,60 @@
 <template>
-  <div class="navbar bg-base-100 get-topup ">
+  <div class="navbar bg-base-100 get-topup">
     <div class="flex-1">
-      <a class="btn btn-ghost normal-case text-3xl rounded-box">Aurora</a><!--打算在这里显示版本信息(或者测试用途)-->
-      <div v-if="globalStore.isAFK" class="badge font-semibold" style="margin-left: 10px;">挂机中</div>
-      <div v-if="!globalStore.isAFK" class="badge font-semibold" style="margin-left: 10px;">计时中</div>
+      <a class="btn btn-ghost normal-case text-3xl rounded-box">Aurora</a
+      ><!--打算在这里显示版本信息(或者测试用途)-->
+      <div
+        v-if="globalStore.isAFK"
+        class="badge font-semibold"
+        style="margin-left: 10px"
+      >
+        挂机中
+      </div>
+      <div
+        v-if="!globalStore.isAFK"
+        class="badge font-semibold"
+        style="margin-left: 10px"
+      >
+        计时中
+      </div>
     </div>
 
-    <div class="flex-none gap-2 font-f" >
+    <div class="flex-none gap-2 font-f">
       <LoaderTriangle></LoaderTriangle>
     </div>
     <div class="dropdown dropdown-end">
-        <label class="btn btn-ghost btn-circle" :class="{'avatar online': !globalStore.isAFK, 'avatar offline': globalStore.isAFK}" tabindex="0">
-          <div class="w-10 rounded-full">
-            <img :src="getUrl('profile.jpg')"/>
-          </div>
-        </label>
-        <ul
-          class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52 justify-between"
-          tabindex="0"
-        >
-          <li>
-            <a @click="jumpTo('Info')">
-              个人信息
-            </a>
-          </li>
-          <li><a @click="jumpTo('Setting')">设置</a></li>
-        </ul>
-      </div>
+      <label
+        class="btn btn-ghost btn-circle"
+        :class="{
+          'avatar online': !globalStore.isAFK,
+          'avatar offline': globalStore.isAFK,
+        }"
+        tabindex="0"
+      >
+        <div class="w-10 rounded-full">
+          <img :src="getUrl('profile.jpg')" />
+        </div>
+      </label>
+      <ul
+        class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+        tabindex="0"
+      >
+        <li>
+          <a class="justify-between" @click="jumpTo('Info')"> 个人信息 </a>
+        </li>
+        <li><a @click="jumpTo('Setting')">设置</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
-import {getUrl} from "../utils/urlUtils";
+import { getUrl } from "../utils/urlUtils";
 import LoaderTriangle from "./LoaderTriangle.vue";
-import {GlobalStore} from "../stores/Global";
-import {router} from "../utils/router";
+import { GlobalStore } from "../stores/Global";
+import { router } from "../utils/router";
 
-const globalStore = GlobalStore()
+const globalStore = GlobalStore();
 
 const jumpTo = (local) => {
   router.push({
