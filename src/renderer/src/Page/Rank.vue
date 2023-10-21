2f57588d6eee5a92f4ac5ac06e883bef40ee9c0b
DAYGood_Time <35392263+DAYGoodTime@users.noreply.github.com>
2023-10-21 10:08:04 +0800

    features: add information card in rank list
    修复了加载动画位置不是很对的问题(bad way)

--- src/renderer/src/Page/Rank.vue
+++ src/renderer/src/Page/Rank.vue
@@ -17,16 +17,16 @@
         :data="UserList"
         :default-sort="{ prop: 'weekTime', order: 'descending' }"
         v-loading="Loading"
+        :element-loading-svg="svgLoading"
       >
         <el-table-column label="姓名" prop="name" min-width="40">
           <template #default="scope">
-            <div style="display: flex; align-items: center">
+            <div style="display: flex; align-items: center" @click="showInformation(scope.row.id)">
               <img
                 :src="scope.row.avatar"
                 height="30"
                 width="30"
                 style="border-radius: 10px"
-                @click="showInformation(scope.row.id)"
               />
               <span style="margin-left: 10px">{{ scope.row.name }}</span>
             </div>
@@ -66,18 +66,18 @@
           >
             <section
               class="flex justify-center items-center w-20 h-20 rounded-full shadow-md bg-gradient-to-r hover:cursor-pointer hover:scale-110 duration-300"
-            ><img :src="getUrl('profile.jpg')" class="rounded-full"></section>
+            ><img :src="UserInformation.avatar" class="rounded-full"></section>
 
             <section class="block border-l border-white m-3">
               <div class="pl-3">
-                
+
                 <h3
                   class="bg-clip-text text-4xl font-bold gradient"
                 >
-                  黄梓聪
+                  {{UserInformation.name}}
                 </h3>
-                <h3 class="text-xl"> 21级   物联网工程1班</h3>
-                <h3 class="text-lg">前端</h3>
+                <h3 class="text-xl"> {{ UserInformation.grade }} {{ UserInformation.major}}</h3>
+                <h3 class="text-lg">{{UserInformation.work_group}}</h3>
               </div>
               <div class="flex gap-3 pt-2 pl-3"></div>
             </section>
@@ -92,23 +92,25 @@
 </template>
 
 <script lang="ts" setup>
-import { formatSecondTime } from "../utils/TimeUtil";
-import { nextTick, onBeforeMount, onMounted, ref } from "vue";
+import {formatSecondTime} from "../utils/TimeUtil";
+import {nextTick, onBeforeMount, onMounted, ref} from "vue";
 import "../assets/css/common.css";
-import { ElNotification } from "element-plus";
-import { GlobalStore } from "../stores/Global";
-import { TimerStore } from "../stores/Timer";
-import { UserTime } from "../api/interfaces/Schema";
-import { intToRoman } from "../utils/NumberUtil";
-import { getAvatarById } from "../api/API";
-import { getUrl } from "../utils/urlUtils";
+import {ElNotification} from "element-plus";
+import {GlobalStore} from "../stores/Global";
+import {TimerStore} from "../stores/Timer";
+import {UserInfo, UserTime} from "../api/interfaces/Schema";
+import {intToRoman} from "../utils/NumberUtil";
+import {getAvatarById, queryUser} from "../api/API";
+import {getUrl} from "../utils/urlUtils";
 
 
 interface Week {
   index: number;
   name: string;
 }
-
+let emptyInformation: UserInfo = {
+  WeekTime: 0, avatar: "", grade: "", id: "", isAdmin: false, major: "", name: "", token: "", work_group: ""
+}
 const globalStore = GlobalStore();
 const timerStore = TimerStore();
 
@@ -120,17 +122,21 @@ let Loading = ref(true);
 let UserList = ref<UserTime[]>();
 let WeekIndex = ref<Week[]>([]);
 let lastXWeek = ref(0);
+let UserInformation = ref<UserInfo>(emptyInformation)
 
 const filterHandler = (value: string, row: UserTime) => {
   return row.id.substring(0, 2) === value;
 };
 onBeforeMount(async () => {
   await loadWeekList();
+  document.getElementsByClassName('circular')[0].attributes[1].value = "0 0 108 108"
   await loadRankList();
 });
 
 onMounted(async () => {
+
   await nextTick();
+
 });
 const GradeFormatter = (row, colum) => {
   return row.uid.substring(0, 2);
@@ -140,9 +146,19 @@ const TimeFormatter = (row, colum) => {
   return formatSecondTime(row[colum.property]);
 };
 const user = ref(null);
-const showInformation = (id: string) => {
-  user.value.showModal();
-  console.log("show");
+const showInformation = async  (id: string) => {
+
+  let res = await queryUser(id)
+  if (!res.success) {
+    ElNotification({
+      title: "请求失败！",
+      message: "系统异常",
+      type: "error"
+    });
+  } else {
+    UserInformation.value = res.data;
+    user.value.showModal();
+  }
 };
 
 const handlePageChange = async (state: boolean) => {
@@ -216,7 +232,60 @@ const loadRankList = async () => {
   Loading.value = false;
 };
 
-//
+const svgLoading = `<circle cx="12.5" cy="12.5" r="12.5">
+        <animate attributeName="fill-opacity"
+         begin="0s" dur="1s"
+         values="1;.2;1" calcMode="linear"
+         repeatCount="indefinite" />
+    </circle>
+    <circle cx="12.5" cy="52.5" r="12.5" fill-opacity=".5">
+        <animate attributeName="fill-opacity"
+         begin="100ms" dur="1s"
+         values="1;.2;1" calcMode="linear"
+         repeatCount="indefinite" />
+    </circle>
+    <circle cx="52.5" cy="12.5" r="12.5">
+        <animate attributeName="fill-opacity"
+         begin="300ms" dur="1s"
+         values="1;.2;1" calcMode="linear"
+         repeatCount="indefinite" />
+    </circle>
+    <circle cx="52.5" cy="52.5" r="12.5">
+        <animate attributeName="fill-opacity"
+         begin="600ms" dur="1s"
+         values="1;.2;1" calcMode="linear"
+         repeatCount="indefinite" />
+    </circle>
+    <circle cx="92.5" cy="12.5" r="12.5">
+        <animate attributeName="fill-opacity"
+         begin="800ms" dur="1s"
+         values="1;.2;1" calcMode="linear"
+         repeatCount="indefinite" />
+    </circle>
+    <circle cx="92.5" cy="52.5" r="12.5">
+        <animate attributeName="fill-opacity"
+         begin="400ms" dur="1s"
+         values="1;.2;1" calcMode="linear"
+         repeatCount="indefinite" />
+    </circle>
+    <circle cx="12.5" cy="92.5" r="12.5">
+        <animate attributeName="fill-opacity"
+         begin="700ms" dur="1s"
+         values="1;.2;1" calcMode="linear"
+         repeatCount="indefinite" />
+    </circle>
+    <circle cx="52.5" cy="92.5" r="12.5">
+        <animate attributeName="fill-opacity"
+         begin="500ms" dur="1s"
+         values="1;.2;1" calcMode="linear"
+         repeatCount="indefinite" />
+    </circle>
+    <circle cx="92.5" cy="92.5" r="12.5">
+        <animate attributeName="fill-opacity"
+         begin="200ms" dur="1s"
+         values="1;.2;1" calcMode="linear"
+         repeatCount="indefinite" />
+    </circle>`
 </script>
 
 <style scoped>
@@ -240,8 +309,11 @@ const loadRankList = async () => {
   --el-table-border-color: hsl(var(--ac) / var(--tw-text-opacity));
   --el-table-bg-color: hsl(var(--b1) / var(--tw-bg-opacity));
   --el-table-tr-bg-color: hsl(var(--b1) / var(--tw-bg-opacity));
-  //--el-table-row-hover-bg-color: hsl(var(--pc) / var(--tw-text-opacity)); --el-table-header-bg-color: hsl(var(--b1) / var(--tw-bg-opacity));;
+  --el-table-row-hover-bg-color: hsl(var(--pc) / var(--tw-text-opacity));
+  --el-table-header-bg-color: hsl(var(--b1) / var(--tw-bg-opacity));;
+  /*
   //--el-tag-bg-color: 年级标签 背景颜色; //--el-tag-text-color: 年级标签 文字颜色;
+   */
 }
 
 :deep(.cell) {
@@ -249,16 +321,21 @@ const loadRankList = async () => {
   font-weight: 700;
   color: hsla(var(--bc) / var(--tw-text-opacity, 1));
 }
+
 .header {
   display: flex;
   align-items: center;
   justify-content: space-between;
 }
 
+:deep(.el-loading-spinner) {
+  top: 20%;
+}
+
 .glassmophism {
   background: rgba(255, 255, 255, .2);
-	-webkit-backdrop-filter: blur(10px);
-	backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
 }
 
 </style>
